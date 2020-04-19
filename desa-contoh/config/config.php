01be1040d20b7b3516a8561f73271a6f71ac5cc1
zainul154 <61271675+zainul154@users.noreply.github.com>
2020-04-19 10:08:49 +0700

    Update config.php

--- desa-contoh/config/config.php
+++ desa-contoh/config/config.php
@@ -27,5 +27,7 @@
 	Kode provinsi sesuai dengan yg di http://pusatkrisis.kemkes.go.id/daftar-kode-provinsi
 */
 	$config['covid_data'] = 1;
-	$config['provinsi_covid'] = 51; // kode provinsi. Comment baris ini untuk menampilkan data Indonesia
+	$config['provinsi_covid'] = 13; // kode provinsi. Comment baris ini untuk menampilkan data Indonesia
+	$config['provinsi_covid'] = 678; // kode provinsi. Comment baris ini untuk menampilkan data Indonesia
+
 	
